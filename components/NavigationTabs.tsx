5048c778f68850d26945d57387d791bccca97f27
xava-dev <dev@xava-dev.com>
2022-10-09 22:36:15 +0200

    Styled navigation tabs

--- components/NavigationTabs.tsx
+++ components/NavigationTabs.tsx
@@ -5,14 +5,14 @@ const NavigationTabs = () => {
   const router = useRouter();
 
   return (
-    <div className="mx-auto flex flex-row mb-6 space-x-6">
+    <div className="mx-auto flex flex-row mb-6">
       <Link href="/">
         <a
           className={`${
             router.pathname == "/"
-              ? "text-white bg-rm/50"
-              : "text-rm/50 hover:bg-rm/10 hover:text-rm"
-          } px-6 py-2 rounded-md transition-all duration-200`}
+              ? "text-rm border-b-2 border-b-rm"
+              : "text-rm/50 hover:border-b-2 border-b-rm/50"
+          } px-6 py-2 transition-all duration-200`}
         >
           Episodes
         </a>
@@ -21,9 +21,9 @@ const NavigationTabs = () => {
         <a
           className={`${
             router.pathname == "/episodes"
-              ? "text-white bg-rm/50"
-              : "text-rm/50 hover:bg-rm/10 hover:text-rm"
-          } px-6 py-2 rounded-md transition-all duration-200`}
+              ? "text-rm border-b-2 border-b-rm"
+              : "text-rm/50 hover:border-b-2 border-b-rm/50"
+          } px-6 py-2 transition-all duration-200`}
         >
           Episodes with Character
         </a>
@@ -32,9 +32,9 @@ const NavigationTabs = () => {
         <a
           className={`${
             router.pathname == "/characters"
-              ? "text-white bg-rm/50"
-              : "text-rm/50 hover:bg-rm/10 hover:text-rm"
-          } px-6 py-2 rounded-md transition-all duration-200`}
+              ? "text-rm border-b-2 border-b-rm"
+              : "text-rm/50 hover:border-b-2 border-b-rm/50"
+          } px-6 py-2 transition-all duration-200`}
         >
           Characters
         </a>
